31deadae5ac830d7233813997c05ca5123224b16
Borja Fourquet <b.fourquet@outlook.es>
2023-03-19 11:50:18 +0100

    ✨ Add raycast that changes the color of selection

--- scenes/3d_mini_sudoku.gd
+++ scenes/3d_mini_sudoku.gd
@@ -6,22 +6,40 @@ var ray;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ray = RayCast3D.new()
+	ray.set_enabled(true)
 	ray.set_name("RayCast_cells")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var mouse = get_viewport().get_mouse_position()
-	var asdf = camera.project_ray_normal(mouse);
+func raycast_from_camera_to_mouse() -> Node:
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_length = 100
+	var from = camera.project_ray_origin(mouse_pos)
+	var to = from + camera.project_ray_normal(mouse_pos) * ray_length
+	var space = get_world_3d().direct_space_state
+	var ray_query = PhysicsRayQueryParameters3D.new()
+	ray_query.from = from
+	ray_query.to = to
+	ray_query.collide_with_areas = true
+	var raycast_result = space.intersect_ray(ray_query)
 	
-	var space_state = get_world_3d().direct_space_state
+	print(raycast_result)
+	
+	return raycast_result.collider
 
-	var query = PhysicsRayQueryParameters3D.create(camera.position, asdf)
-	var result = space_state.intersect_ray(query)
+func _unhandled_input(event):
+	if (
+		event is InputEventMouseButton
+		and event.button_index == MOUSE_BUTTON_LEFT
+		and event.pressed
+	):
 	
-	print(result)
-#    # use global coordinates, not local to node
-#    var query = PhysicsRayQueryParameters2D.create(Vector2(0, 0), Vector2(50, 100))
-#    var result = space_state.intersect_ray(query)
+		var collider = raycast_from_camera_to_mouse()
+		var cell_number_node = collider.get_parent()
+		
+		print(cell_number_node)
+		
+		var cell_number_material = cell_number_node.mesh.material.duplicate();
+		cell_number_material.albedo_color = Color(0.5, 0, 0)
+		cell_number_node.mesh.material = cell_number_material;
 
-	
+

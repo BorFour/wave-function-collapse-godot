014b232649352594d499758cba86fc21afd13180
Borja Fourquet <b.fourquet@outlook.es>
2023-03-19 16:17:40 +0100

    💫 Fix animation setting transition mode properly

--- prefabs/mini_sudoku_cell.gd
+++ prefabs/mini_sudoku_cell.gd
@@ -9,7 +9,7 @@ extends Node3D
 
 @onready var select_mutex = Mutex.new();
 @onready var can_click = true;
-const tween_animation_step_time: float = 0.3;
+const tween_animation_step_time: float = 0.5;
 
 
 func _set_can_click_to_true():
@@ -28,11 +28,12 @@ func _select_number_cell_by_number(num: int):
 
 	cell_to_select.get_selected()
 
-	tween.set_parallel(Tween.TRANS_ELASTIC)
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_parallel(true)
 	tween.tween_property(cell_to_select, "position", Vector3(0, 1, 0.01), tween_animation_step_time)
 	tween.tween_property(cell_to_select, "scale", Vector3(1, 1, 1), tween_animation_step_time)
 
-	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_parallel(false)
 	tween.tween_callback(_set_can_click_to_true)
 
 	set_meta("SelectedNumber", num);
@@ -42,11 +43,12 @@ func _delect_selected_number():
 	var cell_to_select = number_cells[get_meta("SelectedNumber")];
 	var tween = get_tree().create_tween().bind_node(self)
 	
-	tween.set_parallel(Tween.TRANS_ELASTIC)
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_parallel(true)
 	tween.tween_property(cell_to_select, "scale", Vector3(0.5, 0.5, 0.5), tween_animation_step_time)
 	tween.tween_property(cell_to_select, "position", cell_to_select.original_position, tween_animation_step_time)
 	
-	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.set_parallel(false)
 	tween.tween_callback(cell_to_select.get_deselected)
 	tween.tween_callback(_set_can_click_to_true)
 

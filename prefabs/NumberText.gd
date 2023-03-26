26d5ada6f32951f118054c6f55f317823a50ac8a
Borja Fourquet <b.fourquet@outlook.es>
2023-03-26 10:57:46 +0200

    ⚰️ Remove unused method

--- prefabs/NumberText.gd
+++ prefabs/NumberText.gd
@@ -5,9 +5,3 @@ extends Label3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	text = str(ParentNode.get_meta("CellNumber"));
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
